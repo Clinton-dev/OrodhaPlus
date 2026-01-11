20b46a0ca09a4536993c0ca918c20fe546c209c0
Clinton Dev <clintonwambugu@gmail.com>
2026-01-11 22:27:31 +0300

    Add Default Board and Task seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,10 +24,20 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $board = Board::factory()->create();
-        
-        Task::factory(4)->create([
-            'board_id' => $board->id,
+        $board = Board::factory()->create([
+            'name' => 'Default Board',
+            'description' => 'Tasks to keep organized'
         ]);
+
+        $defaultTasks = [
+            ['name' => 'Task in progress', 'icon' => 'clock', 'status' => 'inProgress', 'description' => 'A task currently being worked on'],
+            ['name' => 'Task Completed', 'icon' => 'workout', 'status' => 'completed', 'description' => 'A task that has been finished'],
+            ['name' => "Task Won't Do", 'icon' => 'tea', 'status' => 'wontDo', 'description' => 'A task that will not be completed'],
+            ['name' => 'Task To do', 'icon' => 'study', 'status' => 'pending', 'description' => 'A task waiting to be started'],
+        ];
+
+        foreach ($defaultTasks as $taskData) {
+            Task::create(array_merge($taskData, ['board_id' => $board->id]));
+        }
     }
 }
